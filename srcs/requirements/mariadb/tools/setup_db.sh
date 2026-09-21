2985114bb90253ad77bb6f109f9c4bcbd0a84b9c
Furia25 <valentinledur@gmail.com>
2026-09-21 17:44:40 +0200

    Fix: Better error handling for mariadb

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -17,12 +17,11 @@ mkdir -p /run/mysqld /var/lib/mysql
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -f /var/lib/mysql/.initialized ]; then
-    [ -d /var/lib/mysql/mysql ] || \
-        mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal > /dev/null
+    rm -rf /var/lib/mysql/*
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal > /dev/null
 
     mysqld_safe --datadir=/var/lib/mysql --skip-networking &
     until mysqladmin ping --silent 2>/dev/null; do sleep 1; done
-
     mysql -u root <<EOSQL
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
