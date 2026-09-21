805e19f396483a216e3306ac3055fc2cdd9f19b7
Furia25 <valentinledur@gmail.com>
2026-09-21 16:55:33 +0200

    Fix: Wrong env variable name in wordpress init

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -7,23 +7,31 @@ until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
 done
 
 cd /var/www/html
-if [ ! -f wp-config.php ]; then
 
-    cp /tmp/wp-config.php /var/www/html/wp-config.php
+if [ ! -f wp-config.php ]; then
+    wp config create \
+        --dbname="${DB_NAME}" \
+        --dbuser="${DB_USER}" \
+        --dbpass="${DB_PASSWORD}" \
+        --dbhost="${DB_HOST}" \
+        --allow-root
 
     wp core install \
-        --url="${DOMAIN_NAME}" \
-        --title="Inception" \
+        --url="https://${DOMAIN_NAME}" \
+        --title="${WP_TITLE}" \
         --admin_user="${WP_ADMIN_USER}" \
         --admin_password="${WP_ADMIN_PASSWORD}" \
         --admin_email="${WP_ADMIN_EMAIL}" \
+        --skip-email \
         --allow-root
+
     wp user create \
         "${WP_USER}" "${WP_USER_EMAIL}" \
         --role=author \
         --user_pass="${WP_USER_PASSWORD}" \
         --allow-root
 
+    chown -R www-data:www-data /var/www/html
 else
     echo "WordPress already initialized, starting normally..."
 fi
