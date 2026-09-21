69ec9543ed11065e40d7740c9affbe2b662632bc
Furia25 <valentinledur@gmail.com>
2026-09-21 16:48:33 +0200

    Fix: Wrong env variable name in mariadb init

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -8,14 +8,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     until mysqladmin ping --silent 2>/dev/null; do sleep 1; done
 
     mysql -u root <<-EOSQL
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-        CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+        CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
         FLUSH PRIVILEGES;
 EOSQL
 
-    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+    mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 fi
 
 chown -R mysql:mysql /var/lib/mysql
