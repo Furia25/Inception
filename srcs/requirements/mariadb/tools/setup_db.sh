834e8b848bfb5640b3b69b216f13041d808a7969
Furia25 <valentinledur@gmail.com>
2026-09-22 16:35:22 +0200

    Fix: removed initialized manifest in mariadb script

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -16,9 +16,9 @@ file_env DB_PASSWORD
 mkdir -p /run/mysqld /var/lib/mysql
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
-if [ ! -f /var/lib/mysql/.initialized ]; then
+if [ ! -d /var/lib/mysql/mysql ]; then
     find /var/lib/mysql -mindepth 1 -delete
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal > /dev/null
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal
 
     mysqld_safe --user=mysql --datadir=/var/lib/mysql --skip-networking &
     timeout 60 sh -c 'until mysqladmin ping --silent 2>/dev/null; do sleep 1; done'
@@ -32,7 +32,6 @@ EOSQL
 
     mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
     wait
-    touch /var/lib/mysql/.initialized
 fi
 
 exec mysqld --user=mysql --datadir=/var/lib/mysql
